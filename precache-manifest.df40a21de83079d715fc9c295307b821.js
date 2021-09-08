8fb7f592499a55748b7a3502c7c01c8063d0c673
pengle <pengle@antdu.com>
2021-09-08 17:09:45 +0800

    Updates

---
+++ precache-manifest.df40a21de83079d715fc9c295307b821.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e08390d6cc0a4183a758fd11a50bdc",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "3482c726b2f0a36c5b38",
+    "url": "/reactApp/static/css/2.9b54f130.chunk.css"
+  },
+  {
+    "revision": "2205260c599263d7ce60",
+    "url": "/reactApp/static/css/main.500de5fc.chunk.css"
+  },
+  {
+    "revision": "3482c726b2f0a36c5b38",
+    "url": "/reactApp/static/js/2.a3714fea.chunk.js"
+  },
+  {
+    "revision": "485c2f1ff61fb82f922d56bba3ce6b1a",
+    "url": "/reactApp/static/js/2.a3714fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2205260c599263d7ce60",
+    "url": "/reactApp/static/js/main.245d5a90.chunk.js"
+  },
+  {
+    "revision": "9d131c75ba7366b2d9ef",
+    "url": "/reactApp/static/js/runtime-main.17f78908.js"
+  }
+]);
